cffaa73d494ee4c62c828a512fa313b00929fa23
Vadim <fable919@gmail.com>
2021-08-22 15:43:18 +0300

    react-pizza-HTML-CSS

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './scss/app.scss';
+
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
