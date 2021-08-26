57068814f7f92dec8cb9ee66005c798fab4fde71
VadimTrancelover <fable919@gmail.com>
2021-08-26 21:00:02 +0300

    make active buttons from categories bar menu

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories items = {[
+            <Categories onClickItem={(item) => console.log(item)} items = {[
               'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'
             ]}/>
             <div className="sort">
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
+// class Categories extends Component {
+
+//     state = {
+//       activeItem: 2,
+//     };
+
+//     onSelectItem = (index) => {
+//       this.setState({
+//         activeItem: index,
+//       })
+//     };
+
+//     render() {
+
+//       const {items} = this.props
+
+//       return(
+//         <div>
+//             <div className="categories">
+//               <ul>
+//                 <li className="active">Все</li>
+//                 {
+//                     items.map((item, index) => 
+//                     <li className={this.state.activeItem === index ? 'active' : ''} // тут передаётся в класс li 'active' в зависимости от того, на какой мы кликнули li. Происхолит проверка - если состояние activeItem равен индексу элемента, на который мы нажали, тогда сlassName становится 'active', а значение activeItem получает из метода onSelectItem, где при нажатии на элемент мы получаем его индекс, и соответственно меняем стэйт
+//                     onClick={() => this.onSelectItem(index)} 
+//                     key={`${item}_${index}`}>{item}</li>)
+                       
+//                 }
+//               </ul>
+//             </div>
+//         </div>
+//       )
+//     }
+// }
+
 
 function Categories({items}) {
 
-    console.log(items)
+    const [activeItem, setActiveItem] = useState(null);
+
+    const onSelectItem = (index) => {
+      setActiveItem(index);
+    }
 
     return (
         <div>
             <div className="categories">
               <ul>
-                <li className="active">Все</li>
+                <li className = {activeItem === null ? 'active' : ''} // проверяем на значение null index-a кноаки 'все', и назначаем ей класс active.
+                    onClick={() => onSelectItem(null)}> 
+                      Все
+                </li>
                 {
-                    items.map((item, index) => 
-                    <li key={`${item}_${index}`}>{item}</li>)    
+                    items && items.map((item, index) => 
+                    <li
+                      className = {activeItem === index ? 'active' : ''}// тут передаётся в класс li 'active' в зависимости от того, на какой мы кликнули li. Происхолит проверка - если состояние activeItem равен индексу элемента, на который мы нажали, тогда сlassName становится 'active', а значение activeItem получает из метода onSelectItem, где при нажатии на элемент мы получаем его индекс, и соответственно меняем стэйт
+                      onClick={() => onSelectItem(index)} //вызываем функцию, которая получает индекс элемента и передаёт его в onSelectItem
+                      key={`${item}_${index}`}>
+                      {item}
+                    </li>)    
                 }
               </ul>
             </div>
