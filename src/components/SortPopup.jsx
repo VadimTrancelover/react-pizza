7c15ca97d65f60e3721135add37b573e4bbbc9b3
VadimTrancelover <fable919@gmail.com>
2021-08-27 22:52:47 +0300

    add component Sortpopup

---
+++ src/components/SortPopup.jsx
@@ -0,0 +1,43 @@
+import React,{useState} from 'react'
+
+function SortPopup() {
+
+    const [visiblePopup, setVisiblePopup] = useState(false);
+    console.log(visiblePopup)
+
+    const toggleVisiblePopup = () => {
+        setVisiblePopup(!visiblePopup)
+    }
+    // const[activeItemPopup, setActiveItemPopup] = useState(true)
+
+    return (
+        <div className="sort">
+              <div className="sort__label">
+                <svg
+                  width="10"
+                  height="6"
+                  viewBox="0 0 10 6"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+                    fill="#2C2C2C"
+                  />
+                </svg>
+                <b>Сортировка по:</b>
+                <span onClick={toggleVisiblePopup}>популярности</span>
+              </div> 
+              {/* для того, чтобы отобразить элемент верстки или скрыть его, мы ставим перед вёрсткой переменную, которая в зависимости от логики принимает true или false.Далее используем логический элемент И(&&) И если переменная true, то вёрстка отображается, если false то нет*/}
+              {visiblePopup && (<div className="sort__popup"> 
+                <ul>
+                  <li className="active">популярности</li>
+                  <li>цене</li>
+                  <li>алфавиту</li>
+                </ul>
+              </div>)}
+            </div>
+    )
+}
+
+export default SortPopup;
